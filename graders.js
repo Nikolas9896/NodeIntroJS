3e7961651ccf2c2a39d79cc80e607176bf3e66f7
Nikolas9896 <nhlynka@gmail.com>
2018-11-22 17:10:41 +0200

    Clear from log average method

--- graders.js
+++ graders.js
@@ -5,12 +5,14 @@ function average(param)
     param.forEach( function(score) {
        sum+=score; 
     });
-    console.log(Math.round(sum/lengthParam));
     return Math.round(sum/lengthParam);
 }
 
 var score = [90, 98, 89, 100, 100, 86, 94];
-average(score);
+console.log("Score 1");
+console.log(average(score));
+
 
 var score2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
-average(score2);
+console.log("Score 2");
+console.log(average(score2));
